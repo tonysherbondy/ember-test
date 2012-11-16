07ab1c14691ce41b045ebb02faff6a0427956e1f
Anthony Sherbondy <tony@addepar.com>
2012-11-16 12:27:18 -0800

    test failure

---
+++ test.js
@@ -0,0 +1,63 @@
+var application;
+
+module('Minimal Application Setup', {
+  setup: function() {
+    Ember.$("#qunit-fixture").html("<div id='one'><div id='one-child'>HI</div></div><div id='two'>HI</div>");
+    return Ember.run(function() {
+      application = Ember.Application.create({
+        rootElement: '#one'
+      });
+      return application.initialize();
+    });
+  },
+  teardown: function() {
+    if (application) {
+      return Ember.run(function() {
+        return application.destroy();
+      });
+    }
+  }
+});
+
+test('Ember exists', function() {
+  return ok(Ember, "Ember exists on global namespace");
+});
+
+test("Minimal Application initialized with an application template and injections", function() {
+  var app;
+  Ember.$('#qunit-fixture').html('<script type="text/x-handlebars">Hello {{controller.name}}!</script>');
+  Ember.run(function() {
+    app = Ember.Application.create({
+      rootElement: '#qunit-fixture'
+    });
+  });
+  app.ApplicationController = Ember.Controller.extend({
+    name: 'Kris'
+  });
+  Ember.run(function() {
+    var stateManager;
+    stateManager = Ember.Object.create();
+    return app.initialize(stateManager);
+  });
+  equal(Ember.$('#qunit-fixture').text(), 'Hello Kris!');
+
+  Ember.run(function() {
+    app.destroy();
+  });
+});
+
+module('Another Application Setup', {
+  setup: function() {
+    Ember.$("#qunit-fixture").html("<div id='one'><div id='one-child'>HI</div></div><div id='two'>HI</div>");
+    return Ember.run(function() {
+      application = Ember.Application.create({
+        rootElement: '#one'
+      });
+      return application.initialize();
+    });
+  }
+});
+
+test('Ember still exists', function() {
+  return ok(Ember, "Ember exists on global namespace");
+});
